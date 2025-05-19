529575d98f09c3715e5b5a25340ba7c0bdd25def
ciscoch <77090195+ciscoch@users.noreply.github.com>
2025-05-18 20:26:42 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,61 @@
-
 import { Routes, Route } from "react-router-dom";
-import { Toaster } from "@/components/ui/toaster";
-import Index from "@/pages/Index";
-import AnimalDetails from "@/pages/AnimalDetails";
-import AddAnimal from "@/pages/AddAnimal";
-import Subscription from "@/pages/Subscription";
-import NotFound from "@/pages/NotFound";
-import WeightTrackingPage from "@/pages/WeightTrackingPage";
-import JournalPage from "@/pages/JournalPage";
-import GalleryPage from "@/pages/GalleryPage";
-import ExpensesPage from "@/pages/ExpensesPage";
-import SchedulePage from "@/pages/SchedulePage";
-import UserSettingsPage from "@/pages/UserSettingsPage";
-import FeedReminderPage from "@/pages/FeedReminderPage";
-import SignupPage from "@/pages/SignupPage";
-import LoginPage from "@/pages/LoginPage";
-import FriendsPage from "@/pages/FriendsPage";
-import UploadsPage from "@/pages/UploadsPage";
-import BuyerLoginPage from "@/pages/buyer/BuyerLoginPage";
-import BuyerDashboardPage from "@/pages/buyer/BuyerDashboardPage";
-import BuyerViewUserPage from "@/pages/buyer/BuyerViewUserPage";
-import BuyerViewAnimalPage from "@/pages/buyer/BuyerViewAnimalPage";
-
-function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/animal/:animalId" element={<AnimalDetails />} />
-        <Route path="/add-animal" element={<AddAnimal />} />
-        <Route path="/subscription" element={<Subscription />} />
-        <Route path="/weights" element={<WeightTrackingPage />} />
-        <Route path="/journal" element={<JournalPage />} />
-        <Route path="/gallery" element={<GalleryPage />} />
-        <Route path="/expenses" element={<ExpensesPage />} />
-        <Route path="/schedule" element={<SchedulePage />} />
-        <Route path="/settings" element={<UserSettingsPage />} />
-        <Route path="/feed-reminders" element={<FeedReminderPage />} />
-        <Route path="/friends" element={<FriendsPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/uploads" element={<UploadsPage />} />
-        
-        {/* Buyer Routes */}
-        <Route path="/buyer/login" element={<BuyerLoginPage />} />
-        <Route path="/buyer/dashboard" element={<BuyerDashboardPage />} />
-        <Route path="/buyer/user/:userId" element={<BuyerViewUserPage />} />
-        <Route path="/buyer/animal/:animalId" element={<BuyerViewAnimalPage />} />
-        <Route path="/connect/:inviteCode" element={<BuyerViewUserPage />} />
-        
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <Toaster />
-    </>
-  );
-}
-
-export default App;
+ import { Toaster } from "@/components/ui/toaster";
+ import Index from "@/pages/Index";
+ import AnimalDetails from "@/pages/AnimalDetails";
+ import AddAnimal from "@/pages/AddAnimal";
+ import Subscription from "@/pages/Subscription";
+ import NotFound from "@/pages/NotFound";
+ import WeightTrackingPage from "@/pages/WeightTrackingPage";
+ import JournalPage from "@/pages/JournalPage";
+ import GalleryPage from "@/pages/GalleryPage";
+ import ExpensesPage from "@/pages/ExpensesPage";
+ import SchedulePage from "@/pages/SchedulePage";
+ import UserSettingsPage from "@/pages/UserSettingsPage";
+ import FeedReminderPage from "@/pages/FeedReminderPage";
+ import SignupPage from "@/pages/SignupPage";
++import AccountTypePage from "@/pages/AccountTypePage";
+ import LoginPage from "@/pages/LoginPage";
+ import FriendsPage from "@/pages/FriendsPage";
+ import UploadsPage from "@/pages/UploadsPage";
+ import BuyerLoginPage from "@/pages/buyer/BuyerLoginPage";
+ import BuyerDashboardPage from "@/pages/buyer/BuyerDashboardPage";
+ import BuyerViewUserPage from "@/pages/buyer/BuyerViewUserPage";
+ import BuyerViewAnimalPage from "@/pages/buyer/BuyerViewAnimalPage";
+ 
+ function App() {
+   return (
+     <>
+       <Routes>
+         <Route path="/" element={<Index />} />
+         <Route path="/animal/:animalId" element={<AnimalDetails />} />
+         <Route path="/add-animal" element={<AddAnimal />} />
+         <Route path="/subscription" element={<Subscription />} />
+         <Route path="/weights" element={<WeightTrackingPage />} />
+         <Route path="/journal" element={<JournalPage />} />
+         <Route path="/gallery" element={<GalleryPage />} />
+         <Route path="/expenses" element={<ExpensesPage />} />
+         <Route path="/schedule" element={<SchedulePage />} />
+         <Route path="/settings" element={<UserSettingsPage />} />
+         <Route path="/feed-reminders" element={<FeedReminderPage />} />
+         <Route path="/friends" element={<FriendsPage />} />
+-        <Route path="/signup" element={<SignupPage />} />
++        <Route path="/signup" element={<AccountTypePage />} />
++        <Route path="/signup/exhibitor" element={<SignupPage />} />
+         <Route path="/login" element={<LoginPage />} />
+         <Route path="/uploads" element={<UploadsPage />} />
+         
+         {/* Buyer Routes */}
+         <Route path="/buyer/login" element={<BuyerLoginPage />} />
+         <Route path="/buyer/dashboard" element={<BuyerDashboardPage />} />
+         <Route path="/buyer/user/:userId" element={<BuyerViewUserPage />} />
+         <Route path="/buyer/animal/:animalId" element={<BuyerViewAnimalPage />} />
+         <Route path="/connect/:inviteCode" element={<BuyerViewUserPage />} />
+         
+         <Route path="*" element={<NotFound />} />
+       </Routes>
+       <Toaster />
+     </>
+   );
+ }
+ 
+ export default App;
