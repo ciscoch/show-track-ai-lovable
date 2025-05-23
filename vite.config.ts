e3277e9ed14c72a14fc89c750a329bef28481b46
ciscoch <77090195+ciscoch@users.noreply.github.com>
2025-05-22 22:41:47 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+let lovableTagger
+try {
+  lovableTagger = await import("lovable-tagger");
+} catch (e) {
+  console.warn("lovable-tagger not found; skipping plugin for test environment");
+}
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
